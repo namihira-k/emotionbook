032c2c43f4ecb9579b0e7b682bd493d398e43e3d
namihiran <namihiran@gmail.com>
2015-12-29 10:09:06 +0900

    add home page

---
+++ emotionbook/src/main/java/jp/co/namihira/emotionbook/web/controller/view/TimelineViewController.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2015 Kosuke Namihira All Rights Reserved.
+ */
+package jp.co.namihira.emotionbook.web.controller.view;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * timeline controller
+ */
+@Controller
+public class TimelineViewController extends AbstractViewController {
+
+    @RequestMapping(value = "/timeline", method = GET)
+    public String get() {
+        return "timeline/body";
+    }
+
+}
