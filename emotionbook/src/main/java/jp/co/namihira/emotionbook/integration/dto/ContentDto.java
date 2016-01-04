534363e185ec5dda767e8b58ec941a62bb10c163
namihiran <namihiran@gmail.com>
2016-01-04 23:33:11 +0900

    apply db access

--- emotionbook/src/main/java/jp/co/namihira/emotionbook/integration/dto/ContentDto.java
+++ emotionbook/src/main/java/jp/co/namihira/emotionbook/integration/dto/ContentDto.java
@@ -5,11 +5,15 @@ package jp.co.namihira.emotionbook.integration.dto;
 
 import java.time.LocalDateTime;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
 import lombok.Data;
 
 @Data
 public class ContentDto {
     private int id;
     private String content;
+    @DateTimeFormat(iso = ISO.DATE_TIME)
     private LocalDateTime datetime;
 }
