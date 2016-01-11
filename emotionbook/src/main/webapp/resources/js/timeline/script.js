a7bfdeb585c792163fb953805a5b3bcfb1dcdca9
namihiran <namihiran@gmail.com>
2016-01-11 15:08:02 +0900

    apply vue js

---
+++ emotionbook/src/main/webapp/resources/js/timeline/script.js
@@ -0,0 +1,19 @@
+var contentList = new Vue({
+    el: '#contentList',
+    data: {
+    	contents: [
+    	           {
+    	        	   userId:'なみひら',
+    	        	   content:'お腹すいた(´・ω・`)',
+    	        	   createdAt:'2015/12/31　10:14'
+    	           },
+    	           {
+    	        	   userId:'なみひら',
+    	        	   content:'テストだよー(´・ω・`)',
+    	        	   createdAt:'2016/01/11　10:14'
+    	           }
+    	 ]
+    },
+    methods: {
+    }
+})
