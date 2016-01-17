e8a6f18f1e2011ba67d4f1a82ba5abaec01db345
namihiran <namihiran@gmail.com>
2016-01-17 11:36:45 +0900

    fix get contents js

--- emotionbook/src/main/webapp/resources/js/timeline/script.js
+++ emotionbook/src/main/webapp/resources/js/timeline/script.js
@@ -1,23 +1,24 @@
 var contentList = new Vue({
-    el: '#contentList',
-    data: {
-    	contents: null
-    },
+  el: '#js-content-list',
+  data: {
+    contents: null
+  },
 
-    created: function () {
-    	this.fetchContents()
-    },
+  created: function () {
+    this.fetchContents()
+  },
 
-    methods: {
-    	fetchContents: function () {
-			$.ajax({
-				type : 'GET',
-				url : '/emotionbook/api/contents',
-				dataType : 'json',
-				success : function(data) {
-				    this.contents = data;
-				}
-			});
-    	}
+  methods: {
+    fetchContents: function () {
+      var self = this;
+	  $.ajax({
+	    type : 'GET',
+		url : '/emotionbook/api/contents',
+		dataType : 'json',
+		success : function(data) {
+		  self.contents = data;
+	    }
+	  });
     }
+  }
 })
